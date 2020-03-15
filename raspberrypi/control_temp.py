45c43b60ed85ac9cf5561a54db60fd4c3189cc90
nobody <noreply@github.com>
2020-03-15 22:52:25 +0800

    update

--- raspberrypi/control_temp.py
+++ raspberrypi/control_temp.py
@@ -1,39 +1,51 @@
+#!/usr/bin/python3
+
 import sys
 import time
 
 try:
-        import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO
 except RuntimeError:
-        print("Error importing RPi.GPIO! try to use sudo to run your script")
+    print("Error importing RPi.GPIO! try to use sudo to run your script")
 
 def cpu_temp():
-        with open("/sys/class/thermal/thermal_zone0/temp", 'r') as f:
-                return float(f.read())/1000
+    with open("/sys/class/thermal/thermal_zone0/temp", 'r') as f:
+        return float(f.read())/1000
 
 def main():
-        channel = 16
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+    channel = 16
+    temp_high = 68
+    temp_low  = 50
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
-        GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
-        is_close = True
-        while True:
-                temp = cpu_temp()
-                if is_close:
-                        if temp > 68.0:
-                                print("current temperature: " + str(temp))
-                                print(time.ctime() + " : " + str(temp) + " -> open air fan")
-                                GPIO.output(channel, GPIO.LOW)
-                                is_close = False
+    GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
+    is_close = True
+    while True:
+        temp = cpu_temp()
+        if is_close:
+            if temp > temp_high:
+                print("O",end='',flush=True)
+                GPIO.output(channel, GPIO.LOW)
+                is_close = False
+            else:
+                if temp < temp_low:
+                    print(".",end='',flush=True)
                 else:
-                        if temp < 50.0:
-                                print("current temperature: " + str(temp))
-                                print(time.ctime() + " : " + str(temp) + " -> close air fan")
-                                GPIO.output(channel, GPIO.HIGH)
-                                is_close = True
-                time.sleep(5.0)
+                    print("o",end='',flush=True)
+        else:
+            if temp < temp_low:
+                print(".",end='',flush=True)
+                GPIO.output(channel, GPIO.HIGH)
+                is_close = True
+            else:
+                if temp > temp_high:
+                    print("O",end='',flush=True)
+                else:
+                    print("o",end='',flush=True)
+        time.sleep(5.0)
 
 if __name__ == '__main__':
-        main()  
+    main()  
 
 
