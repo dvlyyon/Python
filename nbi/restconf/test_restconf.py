8182684b450e0f357f00b4382fb511e6f42d0172
dj Yang <dyang@infinera.com>
2021-10-12 16:40:09 +0800

    update

--- nbi/restconf/test_restconf.py
+++ nbi/restconf/test_restconf.py
@@ -6,12 +6,12 @@ def test_maxiumn_session():
     sessions = []
     try:
         for i in range(6):
-            for j in range(20):
-                session = RestconfSession('10.13.16.24','8181',f"admin{i}","e2e!Net4u#")
+            for j in range(200):
+                session = RestconfSession('172.29.202.84','8181',f"admin{i}","e2e!Net4u#")
                 s, r, d = session.connect()
                 print (f"No.:{i}.{j} Status:{s} Reason:{r} Data:{d}")
                 sessions.append(session)
-        time.sleep(300)
+        time.sleep(3600)
     finally:
         for s in sessions:
             s.close()
