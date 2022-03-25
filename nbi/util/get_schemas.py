091157810232ff449e42efb7781f237d18f53879
David Yon <david@mobile>
2022-03-25 11:27:21 +0800

    update

--- nbi/util/get_schemas.py
+++ nbi/util/get_schemas.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import shutil
 import argparse
@@ -21,18 +22,42 @@ def retrieve_schema(ip,port,user,password,yang_dir,pattern):
         if fmt == "yang":
             r, y = netconf_client.get_schema(identifier,version,fmt)
             prefix = ""
-            items = pattern.split("||")
-            if any([t in y.data for t in items]):
-                prefix=f"{pattern.split('||')[0]}."
+            tomatch = re.compile("[\n\r]+\s+namespace[ ]+\"http://(\w+)\.com\S+\";[ ]*[\n\r]+")
+            match = tomatch.search(y.data)
+            if match:
+                com = match.group(1)
+                pattern["com"]=com
+                prefix=f"{com}."
             with open(f"{yang_dir}/{prefix}{identifier}.yang", "w") as f:
                 f.write(y.data)
     netconf_client.close()
 
+def createTree(ip, port, user, password, force, version, yang_dir):
+    if force and os.path.exists(version):
+       shutil.rmtree(version)
+
+    if not os.path.exists(yang_dir):
+        os.mkdir(version)
+        os.mkdir(yang_dir)
+        pattern={}
+        commands={}
+        retrieve_schema(ip=ip,user=user,port=port,password=password,yang_dir=yang_dir,pattern=pattern)
+        com=pattern.get('com') 
+        commands[com]=f"pyang -p {yang_dir} -f jstree -o ./{version}/{com}_{version}.html {yang_dir}/{com}*.yang"
+        commands["ietf"]=f"pyang -p {yang_dir} -f jstree -o ./{version}/ietf_{version}.html {yang_dir}/iana-*.yang {yang_dir}/ietf-*.yang {yang_dir}/libnet*.yang {yang_dir}/n*.yang"
+        commands["openconfig"]=f"pyang -p {yang_dir} -f jstree -o ./{version}/openconfig_{version}.html {yang_dir}/openconfig-*.yang"
+        
+
+        result={}
+        for command in commands:
+            result[command]=subprocess.run(commands[command],shell=True)
+        return result
 
 def run(ip,port,user,password,force,pattern):
     cli_client = SSHSession(ip=ip,user=user,passwd=password)
     cli_client.connect()
     result, output = cli_client.sendCmd_without_connection_retry("swversion")
+    cli_client.close()
     version = None
     commands = []
     if result:
@@ -42,31 +67,11 @@ def run(ip,port,user,password,force,pattern):
             if " Active " in line:
                 version = "R{}_{}".format(columns[5],columns[6])
                 yang_dir = f"{version}/yang"
-                commands.append(f"pyang -p {yang_dir} -f jstree -o ./{version}/private_{version}.html {yang_dir}/{pattern.split('||')[0]}*.yang")
-                commands.append(f"pyang -p {yang_dir} -f jstree -o ./{version}/ietf_{version}.html {yang_dir}/iana-*.yang {yang_dir}/ietf-*.yang {yang_dir}/libnet*.yang {yang_dir}/n*.yang")
-                commands.append(f"pyang -p {yang_dir} -f jstree -o ./{version}/openconfig_{version}.html {yang_dir}/openconfig-*.yang")
-                break
+                return createTree(ip, port, user, password, force, version, yang_dir)
             elif "-active" in line:
                 version = "{}_{}".format(columns[2].split("-")[1],columns[2].split("F-")[1])
                 yang_dir = f"{version}/yang"
-                command = f"pyang -p {yang_dir} -f jstree -o ./{version}/private_{version}.html {yang_dir}/{pattern.split('||')[0]}*.yang"
-                commands.append(command)
-                command = f"pyang -p {yang_dir} -f jstree -o ./{version}/ietf_{version}.html {yang_dir}/iana-*.yang {yang_dir}/ietf-*.yang {yang_dir}/libnet*.yang {yang_dir}/n*.yang"
-                commands.append(command)
-                break
-    cli_client.close()
-
-    if force and os.path.exists(version):
-       shutil.rmtree(version)
-
-
-    if not os.path.exists(yang_dir):
-        os.mkdir(version)
-        os.mkdir(yang_dir)
-        retrieve_schema(ip=ip,user=user,port=port,password=password,yang_dir=yang_dir,pattern=pattern)
-        for command in commands:
-            result=subprocess.run(command,shell=True)
-            print(result)
+                return createTree(ip, port, user, password, force, version, yang_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -75,7 +80,7 @@ if __name__ == "__main__":
     parser.add_argument('-u', '--user_name', required=True, help='The user name to login NE')
     parser.add_argument('-p', '--passwd', required=True, help='The password for user to login NE')
     parser.add_argument('-r', '--refresh', action="store_true", help='force to retrieve schema and translate again') 
-    parser.add_argument('--pattern', required=True, help='is use to distigush private mode')
+    parser.add_argument('--pattern', default='',  help='is use to distigush private mode')
     args = parser.parse_args()
 
     print(args.refresh)
