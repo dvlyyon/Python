35bef65356895b512e02d9bca640e06dfa7f615f
David Yon <david@mobile>
2022-07-04 12:58:58 +0800

    update

---
+++ util/fileutil.py
@@ -0,0 +1,14 @@
+from subprocess import PIPE, Popen
+import sys
+
+def get_command_output(command):
+    with Popen(command, stdout=PIPE, stderr=PIPE, shell=True) as process:
+        output, err = process.communicate(timeout=15)
+        if not process.returncode:
+            output = output.decode("utf-8")
+            return (True, output)
+    return (False,"")
+
+if __name__ == "__main__":
+    r, i = get_command_output(sys.argv[1])
+    print(i.split('\n'))
